6d1d58c093e2fe6af5491e6b7bc5a418ccf49bf5
Sadek Ali <sadekali1530@gmail.com>
2023-10-22 14:43:01 +0600

    successfully add tost

--- src/pages/Add/Add.jsx
+++ src/pages/Add/Add.jsx
@@ -1,75 +1,86 @@
 
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const Add = () => {
 
-    const handlAdd=e=>{
+    const handlAdd = e => {
         e.preventDefault()
-        const form=e.target;
-        const name=form.name.value;
-        const brand=form.brand.value;
-        const type=form.type.value;
-        const img=form.img.value;
-        const about=form.about.value;
-        const price=form.price.value;
-        const rating=form.rating.value;
+        const form = e.target;
+        const name = form.name.value;
+        const brand = form.brand.value;
+        const type = form.type.value;
+        const img = form.img.value;
+        const about = form.about.value;
+        const price = form.price.value;
+        const rating = form.rating.value;
 
-        const product={name,brand,type,img,about,price,rating};
+        const product = { name, brand, type, img, about, price, rating };
         console.log(product)
 
-        fetch('http://localhost:5001/products',{
-            method:"POST",
-            headers:{
-                "content-type":"application/json"
+        fetch('http://localhost:5001/products', {
+            method: "POST",
+            headers: {
+                "content-type": "application/json"
             },
-            body:JSON.stringify(product)
+            body: JSON.stringify(product)
         })
-        .then(res=>res.json())
-        .then(data=>{
-            console.log(data);
-        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                toast.success(' Successfully add ', {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "light",
+                    });
+            })
         // console.log(name,brand,type,img,about,price,rating)
     }
     return (
         <div className='bg-white p-10'>
-            <div>
+            <div className="bg-[#F4F3F0] p-8">
                 <form className='space-y-5' onSubmit={handlAdd} action="">
                     <h1 className='text-4xl font-semibold text-center my-5'>Add products</h1>
                     {/* row -1 */}
                     <div className='flex gap-4 lg:flex-row '>
-                        <div className='w-full'>
+                        <div className='w-full space-y-1'>
                             <h3 className='font-semibold'>Name</h3>
                             <input className='pl-2 py-0.5 w-full' type="text" placeholder='Product Name' required name="name" id="" />
                         </div>
-                        <div className='w-full'>
+                        <div className='w-full space-y-1'>
                             <h3 className='font-semibold'>Brand</h3>
                             <input className='pl-2 py-0.5 w-full' type="text" placeholder='Brand name' name="brand" required id="" />
                         </div>
                     </div>
                     {/* row -2 */}
                     <div className='flex gap-4 lg:flex-row '>
-                        <div className='w-full'>
+                        <div className='w-full space-y-1'>
                             <h3 className='font-semibold'>Type</h3>
                             <input className='pl-2 py-0.5 w-full' type="text" placeholder='Product Type' required name="type" id="" />
                         </div>
-                        <div className='w-full'>
+                        <div className='w-full space-y-1'>
                             <h3 className='font-semibold'>Price</h3>
                             <input className='pl-2 py-0.5 w-full' type="text" placeholder='Price' name="price" required id="" />
                         </div>
                     </div>
                     {/* row -3 */}
                     <div className='flex gap-4 lg:flex-row '>
-                        <div className='w-full'>
+                        <div className='w-full space-y-1'>
                             <h3 className='font-semibold'>Rating</h3>
                             <input className='pl-2 py-0.5 w-full' type="text" placeholder='Rating' required name="rating" id="" />
                         </div>
-                        <div className='w-full'>
+                        <div className='w-full space-y-1'>
                             <h3 className='font-semibold'>Photo</h3>
                             <input className='pl-2 py-0.5 w-full' type="text" placeholder='img url' name="img" required id="" />
                         </div>
                     </div>
 
                     <div className='w-full'>
-                        <h3 className='font-semibold'>Short description</h3>
+                        <h3 className='font-semibold space-y-1'>Short description</h3>
                         <input className=' p-3 w-full' type="text" placeholder='about product' name="about" required id="" />
                     </div>
 
@@ -82,7 +93,20 @@ const Add = () => {
 
                 </form>
             </div>
-
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
+           
+            <ToastContainer />
         </div>
     );
 };
